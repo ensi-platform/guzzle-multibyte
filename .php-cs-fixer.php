954dbdcf1b3f905961ac9681e0637245205a2675
Ms_Natali <golubkova@greensight.ru>
2022-01-21 15:04:31 +0300

    config cs

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,15 +1,9 @@
 <?php
 
 $finder = Symfony\Component\Finder\Finder::create()
-    ->notPath('bootstrap/*')
-    ->notPath('storage/*')
-    ->notPath('resources/view/mail/*')
     ->notPath('vendor')
     ->in(__DIR__)
     ->name('*.php')
-    ->notName('_ide_helper.php')
-    ->notName('_ide_helper_models.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
